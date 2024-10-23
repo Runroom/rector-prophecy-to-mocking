a03336cb2d4aa353ae4f01cdc0f1411845f3f9a4
Alex Sánchez <alejandro@runroom.com>
2024-10-23 17:12:56 +0200

    initial commit

--- src/MigratePhpUnitSetList.php
+++ src/MigratePhpUnitSetList.php
@@ -6,5 +6,5 @@ use Rector\Set\Contract\SetListInterface;
 
 class MigratePhpUnitSetList implements SetListInterface
 {
-    public const PHPUNIT = __DIR__ . '/config/prophecy-to-phpunit.php';
+    public const PHPUNIT = __DIR__ . '../config/sets/phpunit.php';
 }
